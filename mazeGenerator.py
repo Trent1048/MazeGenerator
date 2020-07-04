3067128baf886c3cbdf7e381dd9437a8216ad2b7
Trent1048 <trentbultsma@gmail.com>
2020-07-03 18:41:05 -0700

    add random wall filling without the disjoint set yet

--- mazeGenerator.py
+++ mazeGenerator.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy as np
+import random
 
 def generateMaze(mazeSize, imageSize):
 
@@ -7,14 +8,26 @@ def generateMaze(mazeSize, imageSize):
     if mazeSize % 2 == 0:
         mazeSize += 1
 
+    # setup an array for image creation and fill it with white
     data = np.zeros((mazeSize, mazeSize, 3), dtype=np.uint8)
+    data[0:mazeSize, 0:mazeSize] = [255, 255, 255]
 
-    # create a grid
-    for i in range(mazeSize):
-        for j in range(mazeSize):
+    # draw black on the boarders and walls of the maze
+    for row in range(mazeSize):
+        for col in range(mazeSize):
 
-            if i != 0 and j != 0 and i != mazeSize - 1 and j != mazeSize - 1 and i % 2 != 0 and j % 2 != 0:
-                data[i, j] = [255, 255, 255]
+            # make edges black
+            if row == 0 or col == 0 or row == mazeSize - 1 or col == mazeSize - 1:
+                data[row, col] = [0, 0, 0]
+            # make intersections of walls black
+            elif row % 2 == 0 and col % 2 == 0:
+                data[row, col] = [0, 0, 0]
+            # go through walls
+            elif row % 2 == 0 or col % 2 == 0 and not (row % 2 == 0 and col % 2 == 0):
+                # color them based on earlier calculations using a disjoint set
+                # (the disjoint set will be implimented later, random is a placeholder)
+                if random.random() > 0.5:
+                    data[row, col] = [0, 0, 0]
 
     # turn the array into an image and save it
     img = Image.fromarray(data, 'RGB')
@@ -22,4 +35,4 @@ def generateMaze(mazeSize, imageSize):
     img.save('maze.png')
     img.show()
 
-generateMaze(10, 500)
+generateMaze(11, 500)
