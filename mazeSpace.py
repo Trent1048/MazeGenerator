dae80c83afef9e1b2674aca49dd9289a7f7cdca3
Trent1048 <trentbultsma@gmail.com>
2020-07-05 09:05:05 -0700

    add things for keeping track of the type of space each spot is

---
+++ mazeSpace.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+class SpaceType(Enum):
+
+    edge = 0
+    corner = 1
+    space = 2
+    wall = 3
+
+class MazeSpace:
+
+    def __init__(self, spaceType):
+        self.spaceType = spaceType
+        self.neighbors = []
